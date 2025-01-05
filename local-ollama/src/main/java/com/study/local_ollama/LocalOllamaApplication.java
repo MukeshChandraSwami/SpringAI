1317d352f4fe5efdb50efba61fdb3cd0fedfcb67
Swami, Mukesh <mswami@cvent.com>
2025-01-05 23:28:59 +0530

    Starting interaction with local ollama

---
+++ local-ollama/src/main/java/com/study/local_ollama/LocalOllamaApplication.java
@@ -0,0 +1,13 @@
+package com.study.local_ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalOllamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalOllamaApplication.class, args);
+	}
+
+}
---
+++ local-ollama/src/test/java/com/study/local_ollama/LocalOllamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.local_ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalOllamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
