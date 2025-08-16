2a2b2af7d7d0756299309ebc497f6ba3b05c8ed1
Mukesh Swami <mukesh.swami.501598@gmail.com>
2025-08-16 15:00:12 +0530

    Removed LmStudio service

--- media-details-extractor/src/main/java/com/learn/media_details_extractor/services/MediaService.java
+++ media-details-extractor/src/main/java/com/learn/media_details_extractor/services/MediaService.java
@@ -1,20 +1,11 @@
 package com.learn.media_details_extractor.services;
 
-import com.learn.media_details_extractor.requests.MediaRequest;
 import com.learn.media_details_extractor.response.Response;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface MediaService {
-
-    default Response extractMediaDetails(MediaRequest request) throws Exception {
-        return Response.builder()
-                .success(false)
-                .responseCode(501)
-                .responseMsg("Media details extraction not implemented for this service")
-                .build();
-    }
 
     default Response extractMediaDetails(MultipartFile file) throws Exception {
         return Response.builder()
